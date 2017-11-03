f94e8aa47072b855dc23a607cc3b3208b445f14b
shamim-ahmed <shamim.buet.99@gmail.com>
2017-11-04 10:55:57 +1100

    added more formatting examples

--- string_formatting.py
+++ string_formatting.py
@@ -19,3 +19,23 @@ September: {1}
 October: {2}
 November: {1}
 December: {2}""".format(28, 30, 31))
+
+print("Her age is %d %s, %d %s" % (age, "years", 6, "months"))
+
+for i in range(1, 12):
+	print("No. %2d squared is %4d and cubed is %4d" % (i, i ** 2, i ** 3))
+	
+print("PI is approximately %12.50f" % (22 / 7))
+
+for i in range(1, 12):
+	print("No. {0:2} squared is {1:4} and cubed is {2:4}".format(i, i ** 2, i ** 3))
+	
+print("Left justified result")
+
+for i in range(1, 12):
+	print("No. {0:<2} squared is {1:<4} and cubed is {2:<4}".format(i, i ** 2, i ** 3))
+	
+print("PI is approximately {0:12.50f}".format(22 / 7))
+
+for i in range(1, 12):
+	print("No. {:2} squared is {:4} and cubed is {:4}".format(i, i ** 2, i ** 3))
