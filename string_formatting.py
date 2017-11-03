68634fa4173bd81726208792a43aebecffb459fb
shamim-ahmed <shamim.buet.99@gmail.com>
2017-11-04 10:32:55 +1100

    added new example of string formatting

--- string_formatting.py
+++ string_formatting.py
@@ -4,4 +4,18 @@ age = 36
 print("Ging is " + str(36) + " years old")
 print("Ging is {0} years old".format(age))
 
-print("There are {0} days in {1}, {2}, {3}, {4}, {5}, {6} and {7}".format(31, "January", "March", "May", "July", "August", "October", "December"))
+print("There are {0} days in {1}, {2}, {3}, {4}, {5}, {6} and {7}".format(31, "January", "March", "May", "July", 
+			"August", "October", "December"))
+			
+print("""January: {2}
+February: {0}
+March: {2}
+April: {1}
+May: {2}
+June: {1}
+July: {2}
+August: {2}
+September: {1}
+October: {2}
+November: {1}
+December: {2}""".format(28, 30, 31))
