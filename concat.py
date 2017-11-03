098348ad7650bb40a7148db6221a86abcc51b22a
shamim-ahmed <shamim.buet.99@gmail.com>
2017-11-04 07:29:08 +1100

    added code examples

---
+++ concat.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+
+greeting = 'Hello'
+name = 10
+
+print(greeting + ' ' + str(name))
---
+++ input.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+
+greeting = 'hello'
+name = input('Please tell me your name: ')
+print(greeting + ' ' + name)
+
