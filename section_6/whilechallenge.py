1da6c5821e9a1840267fed3d20a85bfae4b62771
Shamim Ahmed <shamim.buet.cse.99@gmail.com>
2017-11-26 19:39:44 +1100

    added my answer to the while challenge problem

---
+++ section_6/whilechallenge.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+import random
+
+highest = 10
+answer = random.randint(1, highest)
+guess = int(input("Please enter your guess: "))
+
+if guess != answer:
+    done = False
+    
+    while not done:
+        if guess == 0:
+            print("You chose to quit. I oblige...")
+            done = True
+        elif guess == answer:
+            print("Congratulations ! You guessed it !")
+            done = True
+        elif guess < answer:
+            guess = int(input("Please guess higher: "))
+        else:
+            guess = int(input("Please guess lower: "))
+            
+else:
+    print("Wow ! You guessed it FIRST TIME !")
