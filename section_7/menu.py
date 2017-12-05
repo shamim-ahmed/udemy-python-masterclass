d47adf157e3ad0cc2bbe3cde4585d79593338449
Shamim Ahmed <shamim.ahmed@sandstone.com.au>
2017-12-06 10:38:42 +1100

    updated list of food items in menu

--- section_7/menu.py
+++ section_7/menu.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 
 menu = []
-menu.append(["egg", "spam", "bacon"])
+menu.append(["egg", "spam", "bacon", "noodles"])
 menu.append(["egg", "sausage", "bacon"])
 menu.append(["fish", "pizza", "cheese"])
-menu.append(["spam", "rice", "curry"])
+menu.append(["spam", "rice", "curry", "potato"])
 
 for dish in menu:
     if "spam" not in dish:
