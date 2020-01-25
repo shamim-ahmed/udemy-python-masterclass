8168be5b8f212d833f7d799e08558157b3d3e66c
Shamim Ahmed <shamim.buet.99@gmail.com>
2020-01-25 21:01:44 +1100

    minor change in formatting

--- section-3/helloworld2.py
+++ section-3/helloworld2.py
@@ -4,4 +4,10 @@ print("Hello World !")
 print(3+7)
 print(5+9)
 print()
+
+# in this case, a space will be automatically be added after every string
 print("The End", "or is it?", "Keep watching for more about Python", 3)
+
+# if all values to be printed are strings, then we can omit comma
+# in this case, no space character will be automatically added after every string
+print("The End" "or is it?" "Keep watching for more about Python 3")
