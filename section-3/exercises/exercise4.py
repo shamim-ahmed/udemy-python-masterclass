8b14282071b6f930aedab3266777ab5bea6b1eff
Shamim Ahmed <shamim.buet.99@gmail.com>
2021-08-14 23:08:16 +1000

    Add some examples on arithmetic operators

---
+++ section-3/exercises/exercise4.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+
+bun_price = 2.40
+money = 15
+
+no_of_buns = money // bun_price
+print(no_of_buns)
--- section-3/operators.py
+++ section-3/operators.py
@@ -10,7 +10,23 @@ print(a / b)
 print(a // b)
 print(a % b)
 
-print('Printing range...')
+print()
 
 for i in range(1, a // b):
     print(i)
+
+print()
+
+print(a + b / 3 - 4 * 12)
+print(a + (b / 3) - (4 * 12))
+print((((a + b) / 3) - 4) * 12)
+print(((a + b) / 3 - 4) * 12)
+
+c = a + b
+d = c / 3
+e = d - 4
+print(e * 12)
+
+print()
+
+print(a / (b * a) / b)
