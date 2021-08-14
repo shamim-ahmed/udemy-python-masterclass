9d379f46c6ed132d1c8b34120c35a91297f60eb2
Shamim Ahmed <shamim.buet.99@gmail.com>
2021-08-14 21:55:46 +1000

    Add some new examples on string concatenation

--- section-3/strings.py
+++ section-3/strings.py
@@ -1,3 +1,5 @@
+#/usr/bin/env python
+
 print("Today is a good day to learn Python")
 print('Python is fun')
 print("Python's strings are easy to use")
@@ -15,6 +17,13 @@ print(age)
 print(type(greeting))
 print(type(age))
 
-age = '2 years'
-print(age)
-print(type(age))
+# You cannot concatenate an int to a str
+# the following line will cause an error if you uncomment it
+# print(name + ' is ' + age + ' years old')
+
+# formatting is okay
+print("%s is %d years old" % (name, age))
+
+age_in_words = '24 years'
+print(name + ' is ' + age_in_words + ' old')
+
