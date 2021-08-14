ecc351efe8ea839844243c12787e2c14cc116a7d
Shamim Ahmed <shamim.buet.99@gmail.com>
2021-08-14 22:04:16 +1000

    Add an example on arithmetic operators

---
+++ section-3/operators.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+a = 12
+b = 3
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a // b)
+print(a % b)
+
+print('Printing range...')
+
+for i in range(1, a // b):
+    print(i)
