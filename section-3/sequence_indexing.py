bf0c42cb921e9f9decd42a4c6ad80b71483f8477
shamim-ahmed <shamim.buet.99@gmail.com>
2021-08-21 20:14:44 +1000

    Add example on sequence indexing

---
+++ section-3/sequence_indexing.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+computer_parts = ["computer", "monitor", "keyboard", "mouse", "mouse pad"]
+
+# indexing on list
+print(computer_parts[1])
+
+# indexing on item retrieved from list
+print(computer_parts[1][0])
