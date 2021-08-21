4139b4b3488e7094302b170e68f0f9472f07eded
shamim-ahmed <shamim.buet.99@gmail.com>
2021-08-21 19:35:08 +1000

    Add several examples on string slicing

---
+++ section-3/sliceback3.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+letters = "abcdefghijklmnopqrstuvwxyz"
+
+print(letters[16:13:-1])   # qpo
+print(letters[4::-1])      # edcba
+print(letters[:-9:-1])     # zyxwvuts
+print(letters[-4:])        # wxyz
+print(letters[-1:])        # z
+print(letters[:1])         # a
+print(letters[0])          # a  
