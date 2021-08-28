49d316bf6edd12bd29bd9585b934d36fc60f009e
Shamim Ahmed <shamim.buet.99@gmail.com>
2021-08-28 18:28:08 +1000

    Add some new examples on generators

--- section-3/section-12/examples/size3.py
+++ section-3/section-12/examples/size3.py
@@ -2,7 +2,10 @@
 
 import sys
 
+# use built-in range
 big_range = range(10000)
+
+# note how range is converted to a list
 big_list = list(big_range)
 
 print("The size of big range is {} byes".format(sys.getsizeof(big_range)))
