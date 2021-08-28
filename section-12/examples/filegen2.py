6e7f0053b6be92b43eb70da3e1bfaa9f17d5bc3c
Shamim Ahmed <shamim.buet.99@gmail.com>
2021-08-28 20:54:12 +1000

    Add an example of filtering files using extension

---
+++ section-12/examples/filegen2.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import os
+import fnmatch
+
+# define generator for file search
+def find_files(start_dir, extension):
+    for path, directories, files in os.walk(start_dir, topdown = True):
+        for file in fnmatch.filter(files, "*{}".format(extension)):
+            absolute_path = os.path.abspath(path)
+            yield os.path.join(absolute_path, file)
+
+# declare generator for file search
+fgen = find_files(".", ".py")
+
+try:
+    while True:
+        print(next(fgen))
+except StopIteration:
+    pass
