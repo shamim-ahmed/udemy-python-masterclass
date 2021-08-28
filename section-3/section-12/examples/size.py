42fc4b87b9404fec4dac2693bc53c7c5b069133e
Shamim Ahmed <shamim.buet.99@gmail.com>
2021-08-28 18:42:49 +1000

    Minor modification in one example

--- section-3/section-12/examples/size.py
+++ section-3/section-12/examples/size.py
@@ -10,20 +10,18 @@ def my_range(n: int):
         val += 1
 
 _ = input("line 12")
-# big_range = range(5)
 big_range = my_range(1000)
-_ = input("line 15")
+_ = input("line 14")
 
 print("big_range is {} bytes".format(sys.getsizeof(big_range)))
 
 # create a list containing all the values in big_range
 big_list = []
 
-_ = input("line 22")
+_ = input("line 21")
+
 for x in big_range:
     print("line 24 - inside for loop")
     big_list.append(x)
 
 print("big_list is {} bytes".format(sys.getsizeof(big_list)))
-print(big_range)
-print(big_list)
