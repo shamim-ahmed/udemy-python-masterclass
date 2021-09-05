10b026f880e6bea2e047bea371c5eb0343b1e0f2
Shamim Ahmed <shamim.buet.99@gmail.com>
2021-09-05 11:26:54 +1000

    Add an explanatory comment

--- section-4/examples/notin.py
+++ section-4/examples/notin.py
@@ -2,5 +2,6 @@
 
 activity = input("How would you like to spend your time today? ")
 
+# note how caseless comparison is performed
 if "cinema" not in activity.casefold():
     print("But I want to go to the cinema!")
