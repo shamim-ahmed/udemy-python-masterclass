f288860bdbe59494505216a7cd03b3a8bb6620fd
Shamim Ahmed <shamim.buet.99@gmail.com>
2021-09-05 16:54:02 +1000

    Minor modification

--- section-4/examples/adventure.py
+++ section-4/examples/adventure.py
@@ -6,4 +6,8 @@ chosen_exit = ""
 while chosen_exit not in available_exits:
     chosen_exit = input("Please choose a direction: ")
 
+    if chosen_exit == "quit":
+        print("Game over")
+        break
+
 print("Aren't you glad you got out of there?")
