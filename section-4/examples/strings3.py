232bf0855669963fe75e87654b6307f05ea9f2e8
Shamim Ahmed <shamim.buet.99@gmail.com>
2021-09-05 12:38:54 +1000

    Add a new example

---
+++ section-4/examples/strings3.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+number = "9,223;372:036 854,775;807"
+separators = ""
+
+for c in number:
+    if not c.isnumeric():
+        separators += c
+
+values = "".join([c if c not in separators else " " for c in number]).split()
+
+print([int(val) for val in values])
