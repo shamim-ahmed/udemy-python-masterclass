153ac5b8143e6139ca70a09efa2962e32a6caa22
Shamim Ahmed <shamim.buet.99@gmail.com>
2021-09-05 13:58:09 +1000

    Add a new example

---
+++ section-4/examples/conditions3.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+age = int(input("Please enter your age: "))
+
+if age in range(16, 66):
+    print("Enjoy your day at work")
+else:
+    print("Enjoy your free time")
+
+print("-" * 80)
+
+if age < 16 or age > 65:
+    print("Enjoy your free time")
+else:
+    print("Enjoy your day at work")
