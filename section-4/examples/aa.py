d0cbe8ddb9292fb7fea0a9dfbf50e9767ba7f206
Shamim Ahmed <shamim.buet.99@gmail.com>
2021-09-19 22:48:45 +1000

    Add an example involving augmented assignment

---
+++ section-4/examples/aa.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+x = 23
+
+x += 1
+print(x)
+
+x -= 4
+print(x)
+
+x *= 5
+print(x)
+
+x //= 4
+print(x)
+
+x /= 5
+print(x)
+
+x **= 2
+print(x)
+
+x %= 5
+print(x)
+
+greeting = "Good "
+
+greeting += "Morning"
+print(greeting)
+
+greeting *= 5
+print(greeting)
