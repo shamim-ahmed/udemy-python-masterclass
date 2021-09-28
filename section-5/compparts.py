64a987fdea9f84b7bc488caa13378371199f0ffd
Shamim Ahmed <shamim.buet.99@gmail.com>
2021-09-28 23:08:08 +1000

    Add a new example involving list

---
+++ section-5/compparts.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+computer_parts = ["computer",
+                  "monitor",
+                  "keyboard",
+                  "mouse",
+                  "mouse mat"
+                  ]
+
+for part in computer_parts:
+    print(part)
+
+print()
+print(computer_parts[0:3])
+print(computer_parts[2])
+print(computer_parts[-1])
