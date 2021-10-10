007693334996555141ec05a4a5ddc144f79c50e8
Shamim Ahmed <shamim.buet.99@gmail.com>
2021-10-11 00:19:32 +1100

    Add an example involving sorting

---
+++ section-5/examples/sort_example.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+#!/usr/bin/env python3
+
+even = [2, 4, 6, 8]
+print(even)
+another_even = even
+print(another_even)
+print()
+
+even.sort(reverse=True)
+print(even)
+print(another_even)
