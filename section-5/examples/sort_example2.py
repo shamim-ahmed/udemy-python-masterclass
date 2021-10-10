6a386947a6880a37f05c70cd70924ad85074b6ec
Shamim Ahmed <shamim.buet.99@gmail.com>
2021-10-11 00:37:40 +1100

    Add an example involving built-in sorted function

---
+++ section-5/examples/sort_example2.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+even = [2, 4, 6, 8]
+print(even)
+another_even = even
+print(another_even)
+print()
+
+sorted_even = sorted(even, reverse=True)
+print(sorted_even)
+print(another_even)
